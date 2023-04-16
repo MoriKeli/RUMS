e005b3dee95aaa8dc5d664c5451ebd1ca5450c11
brocode <kelimbth@gmail.com>
2023-04-17 01:07:23 +0300

    added new db table field and overrode save() to resize profile pic.

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from PIL import Image
 
 
 class User(AbstractUser):
@@ -13,6 +14,7 @@ class User(AbstractUser):
     marital_status = models.CharField(max_length=10, blank=False)
     religion = models.CharField(max_length=10, blank=False)
     title = models.CharField(max_length=5, blank=False)
+    profile_pic = models.ImageField(upload_to='User-dps/', default='default.jpg')
     is_student = models.BooleanField(default=False)
     is_teachingstaff = models.BooleanField(default=False)
     is_classrep = models.BooleanField(default=False)
@@ -23,6 +25,16 @@ class User(AbstractUser):
 
     def __str__(self):
         return f'{self.username}'
+    
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+    
+        img = Image.open(self.profile_pic.path)
+
+        if img.height > 400 and img.width > 400:
+            output_size = (400, 400)
+            img.thumbnail(output_size)
+            img.save(self.profile_pic.path)
     
     class Meta:
         ordering = ['username']
