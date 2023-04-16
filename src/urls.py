56f731a763b5f065b7970dfea07994c028218faa
brocode <kelimbth@gmail.com>
2023-04-17 01:15:07 +0300

    fixed typo error

--- src/urls.py
+++ src/urls.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+admin.site.site_header = 'Administrator Panel'
+admin.site.site_title = 'School MS'
+admin.site.index_title = 'Karibu tena ...'
 
 urlpatterns = [
+    path('auth/', include('accounts.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
