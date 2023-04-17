89a37ee8fa1725a3084633f2b3f847ee1591846b
brocode <kelimbth@gmail.com>
2023-04-17 13:39:02 +0300

    added new urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-
+from . import views
 
 urlpatterns = [
-    
+    path('login/', views.UserLoginView.as_view(), name='user_login'),
+    path('signup/', views.StudentsSignupView.as_view(), name='signup'),
+    path('staff/signup/staff/', views.StaffSignupView.as_view(), name='staff_signup'),
+    path('nonstaff/signup/', views.NonStaffSignupView.as_view(), name='non_staff_signup'),
+    path('logout/', views.UserLogoutView.as_view(), name='logout_user'),
 ]
