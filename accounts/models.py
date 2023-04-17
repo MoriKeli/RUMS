106df45b35c9233a998af12e2adcf69aa3418ba9
brocode <kelimbth@gmail.com>
2023-04-17 13:45:12 +0300

    added documentation string and comments

--- accounts/models.py
+++ accounts/models.py
@@ -4,6 +4,7 @@ from PIL import Image
 
 
 class User(AbstractUser):
+    """ This is the main User model. """
     id = models.CharField(max_length=20, primary_key=True, unique=True, editable=False)
     email = models.EmailField(unique=True)
     gender = models.CharField(max_length=7, blank=False)
@@ -29,12 +30,12 @@ class User(AbstractUser):
     def save(self, *args, **kwargs):
         super(User, self).save(*args, **kwargs)
     
-        img = Image.open(self.profile_pic.path)
+        img = Image.open(self.profile_pic.path)     # find path of the uploaded image. Note: the image is not saved in the db.
 
-        if img.height > 400 and img.width > 400:
+        if img.height > 400 and img.width > 400:    # check if its greater than 400x400 pixels
             output_size = (400, 400)
-            img.thumbnail(output_size)
-            img.save(self.profile_pic.path)
+            img.thumbnail(output_size)  # resize it according to the values of the output_size
+            img.save(self.profile_pic.path)  # save it in db
     
     class Meta:
         ordering = ['username']
