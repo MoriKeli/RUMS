154ca3f38039061676417c2a16584919e8f1ea88
brocode <kelimbth@gmail.com>
2023-04-17 15:03:53 +0300

    registered new app - students

--- src/settings.py
+++ src/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'students.apps.StudentsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
