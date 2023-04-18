d0d51a449c9643711457a8993b9ec9bbf7a762a3
brocode <kelimbth@gmail.com>
2023-04-18 22:53:19 +0300

    minor change

--- students/urls.py
+++ students/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('admissions/', views.NewStudentsRegistrationView.as_view(), name='student_admission'),
     path('registration/academic/', views.AcademicRegistrationView.as_view(), name='academic_registration'),
-    path('registration/units/', views.UnitRegistrationView.as_view, name='unit_registration'),
+    path('registration/units/', views.UnitRegistrationView.as_view(), name='unit_registration'),
     path('registration/residence/', views.StudentsResidenceRegistrationView.as_view(), name='residence_registration'),
     path('assignment/upload/', views.AssignmentsView.as_view(), name='assignments'),
 
