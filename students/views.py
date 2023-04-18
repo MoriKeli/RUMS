d0f559308c86d415b546b7a886a5e2f108297d90
brocode <kelimbth@gmail.com>
2023-04-18 16:35:35 +0300

    created new views to handle students requests

--- students/views.py
+++ students/views.py
@@ -1,3 +1,99 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
+from .forms import (
+    StudentsRegistrationForm, AcademicRegistrationForm, UnitRegistrationForm, UploadAssignmentsForm, ResidenceRegistrationForm,
+    )
+from .models import AcademicYear
 
-# Create your views here.
+
+class NewStudentsRegistrationView(View):
+    def get(self, request):
+
+        context ={}
+        return render(request, 'students/', context)
+    
+    def post(self, request):
+        form = StudentsRegistrationForm()
+
+        if form.is_valid():
+            student = form.save(commit=False)
+            student.name = request.user
+            student.save()
+
+            messages.success(request, f'{student.name} successfully registered!')
+            return redirect('')
+
+class AcademicRegistrationView(View):
+    def get(self, request):
+
+        context = {}
+        return render(request, 'students/', context)
+    
+    def post(self, request):
+        form = AcademicRegistrationForm()
+
+        if form.is_valid():
+            new_record = form.save(commit=False)
+            new_record.name = request.user.students
+            new_record.save()
+
+            messages.success(request, f'{new_record.name} successfully registered!')
+            return redirect('')
+    
+class UnitRegistrationView(View):
+    def get(self, request):
+
+        context = {}
+        return render(request, 'students/', context)
+    
+    def post(self, request):
+        latest_acad_year = AcademicYear.objects.filter(name=request.user.students).order_by('-academic_year')[0]
+        form = UnitRegistrationForm()
+
+        if form.is_valid():
+            new_record = form.save(commit=False)
+            new_record.student = request.user.students
+            new_record.year = latest_acad_year
+            new_record.save()
+
+            messages.success(request, 'Units successfully registered!')
+            return redirect('')
+        
+class StudentsResidenceRegistrationView(View):
+    def get(self, request):
+
+        context = {}
+        return render(request, 'students/', context)
+
+    def post(self, request):
+        latest_acad_year = AcademicYear.objects.filter(name=request.user.students).order_by('-academic_year')[0]
+        form = ResidenceRegistrationForm()
+
+        if form.is_valid():
+            new_record = form.save(commit=False)
+            new_record.student = request.user.students
+            new_record.year = latest_acad_year
+            new_record.save()
+
+            messages.success(request, 'Residence info. successfully saved!')
+            return redirect('')
+    
+class AssignmentsView(View):
+    def get(self, request):
+
+        context = {}
+        return render(request, 'students/', context)
+    
+    def post(self, request):
+        form = UploadAssignmentsForm()
+
+        if form.is_valid():
+            assignment = form.save(commit=False)
+            assignment.name = request.user.students
+            assignment.save()
+
+            messages.success(request, 'Document successfully uploaded & saved!')
+            return redirect('')
+        
